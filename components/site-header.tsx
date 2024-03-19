c5fefafb9cea6b79e2b69351ee3f475bf2c4483c
Tim Cheung <timshingyu@gmail.com>
2024-03-20 07:00:18 +0800

    [Type]: Introduce text-generate effect component for dynamic text animation.

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,7 +11,7 @@ import { ModeToggle } from "./mode-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <MainNav items={siteConfig.mainNav} />
         <MobileNav />
